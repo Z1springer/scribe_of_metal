e992fd27724e272f01ef5608c39b4072ed2499fa
Zach <zdspringer99@gmail.com>
2021-01-18 20:01:37 -0800

    Full functionality achieved, yes I know I did not commit as much as I should but a lot of progress was made all at once

--- Pieces/server.js
+++ Pieces/server.js
@@ -2,6 +2,7 @@
 // ====================================================
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 
 // Express
 // ====================================================
@@ -13,14 +14,54 @@ app.use(express.json());
 
 // Routes
 // ====================================================
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/index.html"));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 app.get("/notes", (req, res) => {
   console.log(path);
   console.log(__dirname);
   res.sendFile(path.join(__dirname + "/public/notes.html"));
+});
+
+app.get("/api/notes", (req, res) => {
+  fs.readFile("./db.json", function (err, data) {
+    if (err) throw err;
+    var newNoteId = 1;
+    const dataId = JSON.parse(data);
+    dataId.forEach((element) => {
+      element.id = newNoteId++;
+    });
+    fs.writeFile("./db.json", JSON.stringify(dataId), function (err, data) {
+      if (err) throw err;
+    });
+    res.json(dataId);
+  });
+});
+
+app.post("/api/notes", (req, res) => {
+  fs.readFile("./db.json", function (err, data) {
+    if (err) throw err;
+    const newNote = JSON.parse(data);
+    newNote.push(req.body);
+    fs.writeFile("./db.json", JSON.stringify(newNote), function (err, data) {
+      if (err) throw err;
+    });
+    res.json(newNote);
+  });
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  fs.readFile("./db.json", function (err, data) {
+    if (err) throw err;
+    const noteId = req.params.id;
+    const removeNote = JSON.parse(data);
+    removeNote.splice(parseInt(noteId) - 1, 1);
+    fs.writeFile("./db.json", JSON.stringify(removeNote), function (err, data) {
+      if (err) throw err;
+    });
+    res.json(removeNote);
+  });
 });
 
 // Set the server to start listening
